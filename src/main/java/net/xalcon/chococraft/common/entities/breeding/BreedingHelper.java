29cda5ff215210a31b97d4e2569c0078001cf082
Slayer5934 <Slayer5934@aol.com>
2018-07-05 17:24:10 -0400

    Fixed stat and trait chances, you can now get a better chocobo.

--- src/main/java/net/xalcon/chococraft/common/entities/breeding/BreedingHelper.java
+++ src/main/java/net/xalcon/chococraft/common/entities/breeding/BreedingHelper.java
@@ -19,8 +19,9 @@ public class BreedingHelper
     public static EntityChocobo createChild(ChocoboBreedInfo breedInfo, World world)
     {
         EntityChocobo chocobo = new EntityChocobo(world);
-        float traitBaseMod = 0.75f; // TODO: enhance somehow (Items? Parent Happyness?)
+        float traitBaseMod = 0.8f;
         float traitRngLimit = 0.5f;
+        float randomZeroToOne = (float)Math.random();
 
         ChocoboStatSnapshot mother = breedInfo.getMother();
         ChocoboStatSnapshot father = breedInfo.getFather();
@@ -29,31 +30,27 @@ public class BreedingHelper
         chocobo.setLevel(1);
         chocobo.setChocoboColor(ChocoboColor.getRandomColor());
         chocobo.setGeneration(((mother.generation + father.generation) / 2) + 1);
-
-        float health = Math.round((mother.health + father.health) / 2) * (traitBaseMod + (world.rand.nextFloat() * traitRngLimit));
+//Health
+        float health = Math.round((mother.health + father.health) / 2) * (traitBaseMod + ((float)Math.random() * traitRngLimit));
         chocobo.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(Math.min(health, ChocoConfig.breeding.maxHealth));
-
-        float avgSpeed = (mother.speed + father.speed) / 2f;
-        float speed = Math.round(avgSpeed * (traitBaseMod + (world.rand.nextFloat() * traitRngLimit)));
-        speed = MathHelper.clamp(speed, avgSpeed - ChocoConfig.breeding.maxSpeedGrowth, avgSpeed + ChocoConfig.breeding.maxSpeedGrowth);
-
+//Speed
+        float speed = ((mother.speed + father.speed) / 2f) * (traitBaseMod + ((float)Math.random() * traitRngLimit));
         chocobo.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(Math.min(speed, (ChocoConfig.breeding.maxSpeed / 100f)));
-
-        float stamina = Math.round((mother.stamina + father.stamina) / 2) * (traitBaseMod + (world.rand.nextFloat() * traitRngLimit));
+//Stamina
+        float stamina = Math.round((mother.stamina + father.stamina) / 2) * (traitBaseMod + ((float)Math.random() * traitRngLimit));
         chocobo.getEntityAttribute(ChocoboAttributes.MAX_STAMINA).setBaseValue(Math.min(stamina, ChocoConfig.breeding.maxStamina));
+//Traits
+        float canSprintChance = calculateChance(0.03f, 0.25f, 0.5f, mother.canSprint, father.canSprint);
+        chocobo.setCanSprint(canSprintChance > (float)Math.random());
 
-        float canSprintChance = calculateChance(0.05f, 0.15f, 0.4f, mother.canSprint, father.canSprint);
-        chocobo.setCanSprint(canSprintChance > world.rand.nextFloat());
+        float canGlideChance = calculateChance(0.01f, 0.2f, 0.45f, mother.canGlide, father.canGlide);
+        chocobo.setCanGlide(canGlideChance > (float)Math.random());
 
-        float canGlideChance = calculateChance(0.02f, 0.1f, 0.4f, mother.canGlide, father.canGlide);
-        chocobo.setCanGlide(canGlideChance > world.rand.nextFloat());
+        float canDiveChance = calculateChance(0.01f, 0.2f, 0.45f, mother.canDive, father.canDive);
+        chocobo.setCanSprint(canDiveChance > (float)Math.random());
 
-        float canDiveChance = calculateChance(0.02f, 0.1f, 0.3f, mother.canDive, father.canDive);
-        chocobo.setCanSprint(canDiveChance > world.rand.nextFloat());
-
-        float canFlyBaseChance = chocobo.canGlide() ? 0.001f : 0;
-        float canFlyChance = calculateChance(canFlyBaseChance, 0.1f, 0.3f, mother.canFly, father.canFly);
-        chocobo.setCanSprint(canFlyChance > world.rand.nextFloat());
+        float canFlyChance = calculateChance(0.005f, 0.15f, 0.4f, mother.canFly, father.canFly);
+        chocobo.setCanFly(canFlyChance > (float)Math.random());
 
         chocobo.setGrowingAge(-24000);
 
