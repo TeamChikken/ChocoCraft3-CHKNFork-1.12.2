e2a2220baddd0bccbd2efb2e8248c4db79a62fe1
Slayer5934 <Slayer5934@aol.com>
2023-02-08 20:54:32 -0500

    INVENTORY TESTING DO NOT MERGE

--- src/main/java/net/chococraft/common/inventory/ContainerSaddleBag.java
+++ src/main/java/net/chococraft/common/inventory/ContainerSaddleBag.java
@@ -1,5 +1,6 @@
 package net.chococraft.common.inventory;
 
+import net.chococraft.common.entities.EntityChocobo;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
@@ -7,30 +8,48 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 import net.chococraft.common.ChocoConfig;
-import net.chococraft.common.entities.EntityChocobo;
 
 public class ContainerSaddleBag extends Container {
-    public ContainerSaddleBag(EntityChocobo chocobo, EntityPlayer player) {
-        this.refreshSlots(chocobo, player);
-    }
+	private EntityChocobo chocobo;
 
-    public void refreshSlots(EntityChocobo chocobo, EntityPlayer player) {
-        this.inventorySlots.clear();
-        bindPlayerInventory(player);
+	public ContainerSaddleBag(EntityChocobo chocobo, EntityPlayer player) {
+		this.chocobo = chocobo;
+		this.refreshSlots(chocobo, player);
+	}
 
-        switch (chocobo.getSaddleType()) {
-            case SADDLE_BAGS:
-                bindInventorySmall(chocobo.chocoboInventory);
-                break;
-            case PACK:
-                bindInventoryBig(chocobo.chocoboInventory);
-                break;
-        }
+	public EntityChocobo getChocobo() {
+		return chocobo;
+	}
 
-        this.addSlotToContainer(new SlotChocoboSaddle(chocobo.saddleItemStackHandler, 0, -16, 18));
-    }
+	public void refreshSlots(EntityChocobo chocobo, EntityPlayer player) {
+		this.inventorySlots.clear();
 
-    private void bindInventorySmall(IItemHandler inventory) {
+		// Saddle slot
+		this.addSlotToContainer(new SlotChocoboSaddle(chocobo.saddleItemStackHandler, 0, -16, 18));
+		
+		//Player inventory
+		for (int row = 0; row < 3; ++row) {
+			for (int col = 0; col < 9; ++col) {
+				this.addSlotToContainer(new Slot(player.inventory, col + row * 9 + 9, 8 + col * 18, 122 + row * 18));
+			}
+		}
+		for (int i = 0; i < 9; ++i) {
+			this.addSlotToContainer(new Slot(player.inventory, i, 8 + i * 18, 180));
+		}
+		
+		//Chocobo inventory
+		switch (chocobo.getSaddleType()) {
+	    case SADDLE_BAGS:
+	    bindInventorySmall(chocobo.chocoboInventory);
+	    break;
+	    case PACK:
+	    bindInventoryBig(chocobo.chocoboInventory);
+	    break;
+	    }
+	}
+	
+	//Chocobo inventory binding
+	private void bindInventorySmall(IItemHandler inventory) {
     	if (ChocoConfig.chocobo.saddlesCosmeticOnly) return;
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 5; col++) {
@@ -47,26 +66,33 @@ public class ContainerSaddleBag extends Container {
             }
         }
     }
-
-    private void bindPlayerInventory(EntityPlayer player) {
-        for (int row = 0; row < 3; ++row) {
-            for (int col = 0; col < 9; ++col) {
-                this.addSlotToContainer(new Slot(player.inventory, col + row * 9 + 9, 8 + col * 18, 122 + row * 18));
-            }
-        }
-
-        for (int i = 0; i < 9; ++i) {
-            this.addSlotToContainer(new Slot(player.inventory, i, 8 + i * 18, 180));
-        }
-    }
-
+    
     @Override
     public boolean canInteractWith(EntityPlayer playerIn) {
         return true;
     }
 
     @Override
-    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
-        return ItemStack.EMPTY;
-    }
+	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
+		ItemStack itemstack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+			if (index < this.chocobo.chocoboInventory.getSlots()) {
+				if (!this.mergeItemStack(itemstack1, this.chocobo.chocoboInventory.getSlots(), this.inventorySlots.size(), true)) {
+					return ItemStack.EMPTY;
+				}
+			} else 
+				if (!this.mergeItemStack(itemstack1, 0, this.chocobo.chocoboInventory.getSlots(), false)) {
+				return ItemStack.EMPTY;
+			}
+			if (itemstack1.isEmpty()) {
+				slot.putStack(ItemStack.EMPTY);
+			} else {
+				slot.onSlotChanged();
+			}
+		}
+		return itemstack;
+	}
 }
--- src/main/java/net/chococraft/common/inventory/SaddleItemStackHandler.java
+++ src/main/java/net/chococraft/common/inventory/SaddleItemStackHandler.java
@@ -1,7 +1,5 @@
 package net.chococraft.common.inventory;
 
-import javax.annotation.Nonnull;
-
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.INBTSerializable;
@@ -9,73 +7,75 @@ import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemHandlerHelper;
 
+import javax.annotation.Nonnull;
+
 public abstract class SaddleItemStackHandler implements IItemHandler, IItemHandlerModifiable, INBTSerializable<NBTTagCompound> {
-    protected ItemStack itemStack = ItemStack.EMPTY;
+	protected ItemStack itemStack = ItemStack.EMPTY;
 
-    @Override
-    public void setStackInSlot(int slot, @Nonnull ItemStack stack) {
-        ItemStack oldStack = this.itemStack;
-        this.itemStack = stack;
-        // dont update if we change from empty to empty
-        if (!(oldStack.isEmpty() && stack.isEmpty())) {
-            this.onStackChanged();
-        }
-    }
+	@Override
+	public void setStackInSlot(int slot, @Nonnull ItemStack stack) {
+		ItemStack oldStack = this.itemStack;
+		this.itemStack = stack;
+		// dont update if we change from empty to empty
+		if (!(oldStack.isEmpty() && stack.isEmpty())) {
+			this.onStackChanged();
+		}
+	}
 
-    @Override
-    public int getSlots() {
-        return 1;
-    }
+	@Override
+	public int getSlots() {
+		return 1;
+	}
 
-    @Nonnull
-    @Override
-    public ItemStack getStackInSlot(int slot) {
-        return this.itemStack;
-    }
+	@Nonnull
+	@Override
+	public ItemStack getStackInSlot(int slot) {
+		return this.itemStack;
+	}
 
-    @Nonnull
-    @Override
-    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-        if (stack.isEmpty())
-            return ItemStack.EMPTY;
+	@Nonnull
+	@Override
+	public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+		if (stack.isEmpty())
+			return ItemStack.EMPTY;
 
-        if (this.itemStack.isEmpty()) {
-            if (simulate)
-                return ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - 1);
-            this.itemStack = stack.splitStack(1);
-        }
-        this.onStackChanged();
-        return stack;
-    }
+		if (this.itemStack.isEmpty()) {
+			if (simulate)
+				return ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - 1);
+			this.itemStack = stack.splitStack(1);
+		}
+		this.onStackChanged();
+		return stack;
+	}
 
-    @Nonnull
-    @Override
-    public ItemStack extractItem(int slot, int amount, boolean simulate) {
-        if (amount <= 0)
-            return ItemStack.EMPTY;
+	@Nonnull
+	@Override
+	public ItemStack extractItem(int slot, int amount, boolean simulate) {
+		if (amount <= 0)
+			return ItemStack.EMPTY;
 
-        if (simulate)
-            return ItemHandlerHelper.copyStackWithSize(this.itemStack, amount);
-        ItemStack outStack = this.itemStack.splitStack(amount);
-        this.onStackChanged();
-        return outStack;
-    }
+		if (simulate)
+			return ItemHandlerHelper.copyStackWithSize(this.itemStack, amount);
+		ItemStack outStack = this.itemStack.splitStack(amount);
+		this.onStackChanged();
+		return outStack;
+	}
 
-    @Override
-    public int getSlotLimit(int slot) {
-        return 1;
-    }
+	@Override
+	public int getSlotLimit(int slot) {
+		return 1;
+	}
 
-    @Override
-    public NBTTagCompound serializeNBT() {
-        return this.itemStack.writeToNBT(new NBTTagCompound());
-    }
+	@Override
+	public NBTTagCompound serializeNBT() {
+		return this.itemStack.writeToNBT(new NBTTagCompound());
+	}
 
-    @Override
-    public void deserializeNBT(NBTTagCompound nbt) {
-        this.itemStack = new ItemStack(nbt);
-        this.onStackChanged();
-    }
+	@Override
+	public void deserializeNBT(NBTTagCompound nbt) {
+		this.itemStack = new ItemStack(nbt);
+		this.onStackChanged();
+	}
 
-    protected abstract void onStackChanged();
+	protected abstract void onStackChanged();
 }
